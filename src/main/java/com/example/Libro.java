653081f1edc730314e133cf0b1dec1c0ac22dcde
Elgrinch001 <santyelmejor105@gmail.com>
2025-08-12 17:19:16 -0500

    Evaluacion

--- src/main/java/com/example/Libro.java
+++ src/main/java/com/example/Libro.java
@@ -2,23 +2,86 @@ package com.example;
 
 public class Libro {
     // TODO: Declarar atributos privados para titulo, autor y precio
-    
+
+    private String titulo;
+    private String autor;
+    private double precio;
+
     // TODO: Crear constructor con todos los atributos (titulo, autor, precio)
-    
+
+        public Libro(String titulo, String autor, double precio) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.precio = precio;
+    }
+
     // TODO: Crear constructor con solo titulo y autor (precio = 0.0)
-    
+
+        public Libro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.precio = 0.0;
+    }
+
+
     // TODO: Implementar métodos getter para cada atributo
-    
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
     // TODO: Implementar métodos setter para cada atributo
     // Nota: Validar que el precio no sea negativo
-    
+
+     public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setPrecio(double precio) {
+        if (precio >= 0) {
+            this.precio = precio;
+        }
+    }
+
     // TODO: Crear método aplicarDescuento que reciba porcentaje
     // Nota: Calcular descuento y actualizar precio
-    
+
+
+    public void aplicarDescuento(double porcentaje) {
+        if (porcentaje > 0) {
+            this.precio -= this.precio * (porcentaje / 100);
+        }
+    }
+
+
     // TODO: Crear método aplicarDescuento que reciba porcentaje y monto máximo
     // Nota: Aplicar descuento solo si no excede el monto máximo
-    
+
+        public void aplicarDescuento(double porcentaje, double montoMaximo) {
+        double descuento = this.precio * (porcentaje / 100);
+        if (descuento <= montoMaximo) {
+            this.precio -= descuento;
+        }
+    }
+
     // TODO: Crear método mostrarInformacion que imprima los datos del libro
-    
-    
+
+        public void mostrarInformacion() {
+        System.out.println("Título: " + this.titulo);
+        System.out.println("Autor: " + this.autor);
+        System.out.println("Precio: $" + this.precio);
+    }
 }
+
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,15 +5,47 @@ public class Main {
 
         // TODO: Crear método main para:
         // 1. Crear dos objetos Estudiante con diferentes valores
+
+        Estudiante estudiante1 = new Estudiante("Carlos", 20, 8.5);
+        Estudiante estudiante2 = new Estudiante("María", 22, 9.2);
+
         // 2. Mostrar la información de ambos estudiantes
+
+        estudiante1.mostrarInformacion();
+        estudiante2.mostrarInformacion();
+
         // 3. Modificar algún atributo usando un setter
+
+        estudiante1.setPromedio(9.0);
+
         // 4. Mostrar la información actualizada
+
+        estudiante1.mostrarInformacion();
 
         // TODO: Crear método main para:
         // 1. Crear un libro usando el constructor completo
+
+        Libro libro1 = new Libro("El Quijote", "Cervantes", 25.0);
+
         // 2. Crear un libro usando el constructor con solo titulo y autor
+
+        Libro libro2 = new Libro("Cien Años de Soledad", "García Márquez");
+
         // 3. Mostrar información de ambos libros
+
+        libro1.mostrarInformacion();
+        libro2.mostrarInformacion();
+
         // 4. Aplicar descuento usando ambas versiones del método
+
+        libro1.aplicarDescuento(10); // Descuento del 10%
+        libro2.setPrecio(30.0);
+        libro2.aplicarDescuento(15, 5.0);
+
         // 5. Mostrar información actualizada de los libros
+
+        libro1.mostrarInformacion();
+        libro2.mostrarInformacion();
     }
+
 }
